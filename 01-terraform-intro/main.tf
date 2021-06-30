7eed766baf01d5a184edcb22978aef24f0422a06
Salvatore Campagna <salvatorecampagna@gmail.com>
2021-06-30 21:36:00 +0200

    First simple Terraform script

---
+++ 01-terraform-intro/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"
+  profile = "terraform-user"
+}
+
+resource "aws_instance" "ec2-instance0" {
+  ami = "ami-09dd2e08d601bff67"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ec2-instance0"
+    Author = "Salvatore Campagna (salvatore.campagna@acmelabs.com)"
+    Organization = "Acme Labs"
+  }
+}
