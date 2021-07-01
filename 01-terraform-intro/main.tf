8a064dcb684ceea66b4d2597564a9674782eb9da
Salvatore Campagna <salvatorecampagna@gmail.com>
2021-07-01 20:07:56 +0200

    Use 'required_provider' to silence the warning

--- 01-terraform-intro/main.tf
+++ 01-terraform-intro/main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "2.12.0"
   region = "us-west-2"
   profile = "terraform-user"
 }
